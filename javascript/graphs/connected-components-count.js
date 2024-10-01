3de7cb82b62ce67c98eda354443d52e2175b2820
Diana Le <dianapnle@gmail.com>
2024-10-01 02:18:33 -0700

    updated solution for connected components"

--- javascript/graphs/connected-components-count.js
+++ javascript/graphs/connected-components-count.js
@@ -1,23 +1,30 @@
 const connectedComponentsCount = (graph) => {
-    const visited = new Set();
-    let count = 0;
-    for (let node in graph) {
-      if (explore(graph, node, visited) === true) {
-        count += 1;
-      }
-    }
-    return count;
+  let count = 0;
+  let visited = new Set();
+  //increment count by 1 when completing a traversal
+
+  //hop to diff connectedComponents
+  //it is in graph because iterating over a js object
+  for (let node in graph) {
+    if (explore(graph, node, visited) === true) count += 1
+  }
+  return count;
+};
+
+
+//depth traversal or breadth traveral as far as possible or recursive
+const explore = (graph, current, visited) => {
+  //visited to prevent cycles
+  if (visited.has(String(current))) {
+    return false;
   };
+  visited.add(String(current))
+  //iterate through every neighbor
+  for (let neighbor of graph[current]) {
+    explore(graph, neighbor, visited);
+  }
+//return true here because assuming if it gets here you have explored all neighbors possible
+  return true;
+}
 
-
-  const explore = (graph, current, visited) => {
-    if (visited.has(String(current))) return false;
-
-    visited.add(String(current));
-
-    for (let neighbor of graph[current]) {
-      explore(graph, neighbor, visited);
-    }
-
-    return true;
-  };
+//time is O(e) and space is O(n)
