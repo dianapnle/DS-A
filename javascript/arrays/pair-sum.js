92322196e365f9309770b590d58ca65bb1424b55
Diana Le <dianapnle@gmail.com>
2024-10-02 15:17:28 -0700

    added another solution to pair sum

--- javascript/arrays/pair-sum.js
+++ javascript/arrays/pair-sum.js
@@ -46,3 +46,25 @@ const pairSum = (numbers, targetSum) => {
       }
     }
   };
+
+
+
+  ///neetcode solution with map that is technically faster and has more features
+  const twoSum = (nums, target) => {
+    const map = new Map();
+
+    for (let index = 0; index < nums.length; index++) {
+        const num = nums[index];
+        const complement = target - num;
+        const sumIndex = map.get(complement);
+
+        const isTarget = map.has(complement);
+        if (isTarget) {
+            return [index, sumIndex];
+        }
+
+        map.set(num, index);
+    }
+
+    return [-1, -1];
+}
