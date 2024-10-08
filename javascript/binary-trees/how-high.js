a6fb972df219b7e9484ab0a3d8afac0b22ec65ea
Diana Le <dianapnle@gmail.com>
2024-10-08 02:49:33 -0700

    solution to depth of binary tree

--- javascript/binary-trees/how-high.js
+++ javascript/binary-trees/how-high.js
@@ -7,3 +7,56 @@ const howHigh = (node) => {
     return 1 + Math.max(howHigh(node.left), howHigh(node.right))
   };
   //time is O(n) and space is O(n) due to call stack
+
+  ///breadth first
+  class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root) {
+
+        if (root === null) return 0
+
+        let level = 0;
+        //BFS
+        let queue = [root];
+        while (queue.length !== 0) {
+            for (let i = 0; i < queue.length; i ++) {
+                //add all the children on this level
+                let current = queue.shift();
+                if (current.left) {
+                    queue.push(current.left)
+                }
+                if (current.right) {
+                    queue.push(current.right)
+                }
+            }
+            level += 1
+        }
+        return level
+    }
+}
+
+//depth first
+class Solution {
+  /**
+   * @param {TreeNode} root
+   * @return {number}
+   */
+  maxDepth(root) {
+
+      if (root === null) return 0
+      let max = 1;
+      //depth first
+      let stack = [{node:root, level: 1}];
+      while (stack.length !== 0) {
+          let {node, level} = stack.pop();
+          if(level > max) max = level;
+          //add the children if node right and left exist
+          if(node.left) stack.push({node: node.left, level: level + 1});
+          if (node.right) stack.push({node: node.right, level: level + 1})
+      }
+      return max;
+  }
+}
