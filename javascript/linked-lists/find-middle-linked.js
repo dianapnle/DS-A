d63b63b991363fa02cddcd34cb8f5d37b622fcf8
Diana Le <dianapnle@gmail.com>
2024-10-08 00:04:35 -0700

    solution to find middle of linked lists

---
+++ javascript/linked-lists/find-middle-linked.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    //fast and slow pointer
+    //slow moves one position at a time
+    //fast one moves two positions at a time until there is no next position
+    //slow should be at the middle of the linked lists at the end of the traversal
+    let slow = head;
+    let fast = head;
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow
+};
