0c8c2e231a0292ce5e3b8b9919ef96146958a21d
Diana Le <dianapnle@gmail.com>
2024-10-08 12:24:10 -0700

    solution to binary tree diameter

---
+++ javascript/binary-trees/bianry-tree-diameter.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    diameterOfBinaryTree(root) {
+    // Initialize a list to hold the maximum diameter encountered
+        let res = [0];
+    // Call the diameter function starting from the root
+        this.dfs(root, res);
+    //return the maximum diameter encountered
+        return res[0]
+    }
+
+    //returns height
+    dfs(root, res) {
+        if (root === null) return 0;
+    // Recursively calculate the diameter of left and right subtrees
+        let left = this.dfs(root.left);
+        let right = this.dfs(root.right);
+    //update the maximum diameter encountered so far
+        res[0] = Math.max(res[0], left + right);
+    //return the height of the current node
+        return 1 + Math.max(left, right)
+    }
+}
+
+//time and space is O(n)
