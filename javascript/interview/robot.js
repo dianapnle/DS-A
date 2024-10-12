b5b8f1690b6d4769ceb9540aca83fd4ffde333e5
Diana Le <dianapnle@gmail.com>
2024-10-12 00:01:37 -0700

    comments on robot q

--- javascript/interview/robot.js
+++ javascript/interview/robot.js
@@ -1,4 +1,7 @@
+
+
 function profit(logs) {
+    //two queue stacks for each robot type
     let res = [];
     let robot1 = [];
     let robot2 = [];
@@ -7,6 +10,7 @@ function profit(logs) {
     for (let log of logs) {
         const [type, robot, num, price, upgradePrice] = log
         if (type === "supply") {
+            //if its supply, push num amount of robots into queue
             if (robot === "robot1") {
                 for (i = 0; i < num; i ++) {
                     robot1.push(price)
@@ -16,6 +20,7 @@ function profit(logs) {
                     robot1.push(price)
                 }
             }
+            //if sell, check which robot and take from the front of the queue
         } else if (type === "sell") {
             if (robot === "robot1") {
                 let add = robot1.shift();
@@ -24,6 +29,7 @@ function profit(logs) {
                 let add = robot2.shift();
                 res.push(add)
             }
+            //if upgrade , check which robot and replace the last robot in the queue with new price
         } else if (type === "upgrade") {
             if (robot === "robot1") {
                 robot1[robot1.length - 1] = upgradePrice
