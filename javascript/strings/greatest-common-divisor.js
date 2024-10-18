78fdd7469ab897b62f50bb99d204aec9b70bdf20
Diana Le <dianapnle@gmail.com>
2024-10-18 16:31:24 -0700

    updated comments

--- javascript/strings/greatest-common-divisor.js
+++ javascript/strings/greatest-common-divisor.js
@@ -5,7 +5,7 @@ var gcdOfStrings = function(str1, str2) {
 
     let isDivisor = function(length) {
         //check if length is a factor of both strings
-        //if its not a factor of the string (modulo is NOT 0)
+        //if its not a factor of the string, return false immediately (modulo is NOT 0)
         if (m % length !== 0 || n % length !== 0) {
             return false
         }
@@ -19,7 +19,7 @@ var gcdOfStrings = function(str1, str2) {
 
     //want to start at the end of the smallest one and iterate backwards to find the largest substring
     for (let i = Math.min(m, n); i >= 0; i--) {
-        //helper function because if result has to be a divisor of str1 and str2, arbitrarily pick str1,
+        //helper function because if result has to be a divisor of str1 and str2, arbitrarily use str1 but can use either
         if (isDivisor(i)) {
             return str1.slice(0, i)
         }
