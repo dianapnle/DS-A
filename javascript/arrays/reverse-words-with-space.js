82439fa7d6b0844d3f8e9a41156991bbb93b20a3
Diana Le <dianapnle@gmail.com>
2024-12-05 20:17:35 -0800

    solution to reverse words with space

---
+++ javascript/arrays/reverse-words-with-space.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    //only have space in between words
+    //split at each space
+    //reverse
+    let res = [];
+    let split = s.split(" ");
+
+    for (let i = split.length - 1; i >= 0; i--) {
+        //checks if the string is not an empty string. This will filter out:
+        // Empty strings
+        // Strings containing only whitespace (spaces, tabs, etc.)
+        if (split[i] !== "") {
+            res.push(split[i]);
+        }
+    }
+
+    return res.join(" ")
+};
