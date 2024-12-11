80d65f00a7072685b750705c77c082fb32b200e9
Diana Le <dianapnle@gmail.com>
2024-12-11 00:38:11 -0800

    updated notes

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,1 +1,14 @@
-//for most frequent characteres -> use object hash map
+//for most frequent characters, pair sums, -> use object hash map
+    //if it does not already exist in the object -> set to 0, else increase by 1
+    //use object to store previous numbers or values already seen
+
+//linked lists
+    //has head and tail with the tail pointing to null
+    //has order
+    //not required to have contiguous memory like arrays
+
+
+//graphs
+    //has depth first or breadth first
+    //depth first is stack -> goes in one direction all the way until the next
+    //breadth first is queue -> goes to all neighbors first
