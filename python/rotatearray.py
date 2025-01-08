1f962ab29fab05d20d6f22ffedcb9e60c0e15a2c
Diana Le <dianapnle@gmail.com>
2025-01-08 15:43:51 -0800

    rotate array brute force

---
+++ python/rotatearray.py
@@ -0,0 +1,30 @@
+#Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+
+
+#Example 1:
+
+##Input: nums = [1,2,3,4,5,6,7], k = 3
+#Output: [5,6,7,1,2,3,4]
+#Explanation:
+#rotate 1 steps to the right: [7,1,2,3,4,5,6]
+#rotate 2 steps to the right: [6,7,1,2,3,4,5]
+#rotate 3 steps to the right: [5,6,7,1,2,3,4]
+#Example 2:
+
+##Input: nums = [-1,-100,3,99], k = 2
+#Output: [3,99,-1,-100]
+#Explanation:
+##rotate 1 steps to the right: [99,-1,-100,3]
+#rotate 2 steps to the right: [3,99,-1,-100]
+
+#brute force
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        # speed up the rotation
+        k %= len(nums)
+
+        for i in range(k):
+            previous = nums[-1]
+            for j in range(len(nums)):
+                nums[j], previous = previous, nums[j]
